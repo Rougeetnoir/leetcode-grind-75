550eef6816a05ff3fb7e97d24b83421129dbf6ab
Rougeetnoir <yangyaner1@gmail.com>
2026-01-21 13:55:35 -0800

    two pointers_344_Reverse_String

---
+++ patterns/two_pointers/344_Reverse_String/344_Reverse_String.py
@@ -0,0 +1,12 @@
+class Solution:
+    def reverseString(self, s: list[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        l, r = 0, len(s)-1
+        while l < r:
+            val = s[l]
+            s[l] = s[r]
+            s[r] = val
+            l += 1
+            r -= 1
